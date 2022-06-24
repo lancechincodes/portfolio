836c35f989e96e02789f3bb24415467a04131b6f
lancechincodes <lanceechin@gmail.com>
2022-06-24 00:15:28 -0500

    Add mouseover/mouseout functionality to resume grid

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
-console.log("Successfully connected!")
+/*--------------CONSTANT VALUES--------------*/
+
+
+/*--------------STATE VALUES--------------*/
+
+
+/*--------------CACHED ELEMENT REFERENCES--------------*/
+const resumeGrid = document.querySelector('#resume-grid')
+
+/*--------------FUNCTIONS--------------*/
+const onMouseOverResume = function(e) {
+    console.log(e.target.getAttribute('class'))
+    if (e.target.getAttribute('class') !== 'rectangle')
+    {
+        e.target.classList.remove('hide-border')
+    }
+}
+
+const onMouseOutResume = function(e) {
+    if (e.target.getAttribute('class') !== 'rectangle')
+    {
+        e.target.classList.add('hide-border')
+    }
+}
+
+/*--------------EVENT LISTENERS--------------*/
+resumeGrid.addEventListener('mouseover', onMouseOverResume)
+resumeGrid.addEventListener('mouseout', onMouseOutResume)
+
+
