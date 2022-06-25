7f4c7d1676a6de6fbef32f50fe72e99a4e68dafb
lancechincodes <lanceechin@gmail.com>
2022-06-24 19:10:35 -0500

    Add modals with functional close buttons to all sections of resume

--- script.js
+++ script.js
@@ -5,19 +5,26 @@
 
 
 /*--------------CACHED ELEMENT REFERENCES--------------*/
+// Navigation buttons
 const navIconsDiv = document.querySelector('#nav-icons-div')
 const navWordsDiv = document.querySelector('#nav-words-div')
-
 const aboutButton = document.querySelector('#about-button')
 const resumeButton = document.querySelector('#resume-button')
 const projectsButton = document.querySelector('#projects-button')
 const interestsButton = document.querySelector('#interests-button')
 
+// Resume buttons
 const resumeGrid = document.querySelector('#resume-grid')
-const educationalModal = document.querySelector('#educational-modal')
-const modalCloseButton = document.querySelector('.modal-close-button')
+const educationModal = document.querySelector('#education-modal')
+const skillsModal = document.querySelector('#skills-modal')
+const workExperienceModal = document.querySelector('#work-experience-modal')
+const certificationsModal = document.querySelector('#certifications-modal')
+const leadershipModal = document.querySelector('#leadership-modal')
+const honorsAndAwardsModal = document.querySelector('#honors-and-awards-modal')
+const modalCloseButton = document.querySelectorAll('.modal-close-button')
 
 /*--------------FUNCTIONS--------------*/
+// Navaigation functions
 const onMouseOverNavIcons = function(e) {
     if (e.target.getAttribute('id') === 'home-button') {
         e.target.setAttribute('src', 'images/logos/lc-logo-alt.png')
@@ -29,7 +36,6 @@ const onMouseOverNavIcons = function(e) {
         e.target.setAttribute('src', 'images/logos/linkedin-logo-alt.png')
     }
 }
-
 const onMouseOutNavIcons = function(e) {
     if (e.target.getAttribute('id') === 'home-button') {
         e.target.setAttribute('src', 'images/logos/lc-logo.png')
@@ -41,7 +47,6 @@ const onMouseOutNavIcons = function(e) {
         e.target.setAttribute('src', 'images/logos/linkedin-logo.png')
     }
 }
-
 const onMouseOverNavWords = function(e) {
     if (e.target.getAttribute('id') === 'about-button') {
         resumeButton.style.opacity = .5
@@ -64,7 +69,6 @@ const onMouseOverNavWords = function(e) {
         projectsButton.style.opacity = .5
     }
 }
-
 const onMouseOutNavWords = function(e) {
     aboutButton.style.opacity = 1
     resumeButton.style.opacity = 1
@@ -72,42 +76,71 @@ const onMouseOutNavWords = function(e) {
     interestsButton.style.opacity = 1
 }
 
+// Resume functions
 const onMouseOverResume = function(e) {
     if (e.target.getAttribute('class') !== 'rectangle')
     {
         e.target.classList.remove('hide-border')
     }
 }
-
 const onMouseOutResume = function(e) {
     if (e.target.getAttribute('class') !== 'rectangle')
     {
         e.target.classList.add('hide-border')
     }
 }
-
 const onClickResume = function(e) {
     if (e.target.getAttribute('id') === 'education') {
-        educationalModal.style.display = 'block'
+        educationModal.style.display = 'block'
         resumeGrid.style.opacity = .5 // semi-transparent
     } 
+    else if (e.target.getAttribute('id') === 'skills') {
+        skillsModal.style.display = 'block'
+        resumeGrid.style.opacity = .5 // semi-transparent
+    }
+    else if (e.target.getAttribute('id') === 'work-experience') {
+        workExperienceModal.style.display = 'block'
+        resumeGrid.style.opacity = .5 // semi-transparent
+    }
+    else if (e.target.getAttribute('id') === 'certifications') {
+        certificationsModal.style.display = 'block'
+        resumeGrid.style.opacity = .5 // semi-transparent
+    }
+    else if (e.target.getAttribute('id') === 'leadership') {
+        leadershipModal.style.display = 'block'
+        resumeGrid.style.opacity = .5 // semi-transparent
+    }
+    else if (e.target.getAttribute('id') === 'honors-and-awards') {
+        honorsAndAwardsModal.style.display = 'block'
+        resumeGrid.style.opacity = .5 // semi-transparent
+    }
 }
-
 const closeModal = function() {
-    educationalModal.style.display = 'none'
+    // Hide all modals
+    educationModal.style.display = 'none'
+    skillsModal.style.display = 'none'
+    workExperienceModal.style.display = 'none'
+    certificationsModal.style.display = 'none'
+    leadershipModal.style.display = 'none'
+    honorsAndAwardsModal.style.display = 'none'
     resumeGrid.style.opacity = 1 // fully opaque
 }
 
 /*--------------EVENT LISTENERS--------------*/
+// Navigation event listeners
 navIconsDiv.addEventListener('mouseover', onMouseOverNavIcons)
 navIconsDiv.addEventListener('mouseout', onMouseOutNavIcons)
-
 navWordsDiv.addEventListener('mouseover', onMouseOverNavWords)
 navWordsDiv.addEventListener('mouseout', onMouseOutNavWords)
 
+// Resume event listeners
 resumeGrid.addEventListener('mouseover', onMouseOverResume)
 resumeGrid.addEventListener('mouseout', onMouseOutResume)
 resumeGrid.addEventListener('click', onClickResume)
-modalCloseButton.addEventListener('click', closeModal)
+
+// add event listeners to ALL modal buttons
+for (let i = 0; i < modalCloseButton.length; i++) {
+    modalCloseButton[i].addEventListener('click', closeModal)
+}
 
 
